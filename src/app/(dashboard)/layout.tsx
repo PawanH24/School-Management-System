25b90681532938672bfac960d5ab6bd118c28f29
PawanH24 <98205519+PawanH24@users.noreply.github.com>
2026-03-14 09:07:37 +0545

    Change logo text

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
       <div className="w-[15%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-white p-4 overflow-y-auto ">
         <Link href="/" className="flex items-center justify-center gap-2 ">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden md:block text-black">LotusSoft</span>
+          <span className="hidden md:block text-black">School</span>
         </Link>
         <Menu />
       </div>
